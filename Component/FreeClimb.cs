6d0c2245d6478d19887fdd830eee0e738b128d54
Wully <wully@robb.scot>
2022-01-23 00:31:36 +0000

    Added FreeClimb option

---
+++ Component/FreeClimb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using GameModeLoader.Module;
+using ThunderRoad;
+using UnityEngine;
+using UnityEngine.Events;
+using GameModeLoader.Utils;
+using Wully.Utils;
+
+namespace GameModeLoader.Component {
+	public class FreeClimb : LevelModule {
+		
+		public override IEnumerator OnLoadCoroutine() {
+
+			if (Level.current.GetOptionAsBool("freeclimb", true)) {
+				RagdollHandClimb.climbFree = true;
+			}
+
+			yield return base.OnLoadCoroutine();
+		}
+
+		public override void OnUnload() {
+			if ( Level.current.GetOptionAsBool("freeclimb", true) ) {
+				RagdollHandClimb.climbFree = false;
+			}
+			base.OnUnload();
+		}
+	}
+}
